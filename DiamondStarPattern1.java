02b70888e2516b58ca6bbaab577d618bfcef231d
Nithin <nithinvskk@gmail.com>
2026-08-15 08:46:06 +0530

    Add files via upload

---
+++ DiamondStarPattern1.java
@@ -0,0 +1,29 @@
+package dsa;
+
+import java.util.*;
+public class DiamondStarPattern1 {
+	 public static void main(String[] args) {
+	        Scanner nt = new Scanner(System.in);
+	        System.out.print("Enter the n : ");
+	        int n = nt.nextInt();
+	        int i,j,k,p,q,r;
+	        for(i=1;i<=n;i++) {
+	            for(j=1;j<=n-i;j++) {
+	                System.out.print(" ");
+	            }
+	            for(k=1;k<=2*i-1;k++) {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	        for(p=n-1;p>0;p--) {
+	            for(q=1;q<=n-p;q++) {
+	                System.out.print(" ");
+	            }
+	            for(r=1;r<=2*p-1;r++) {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	    }
+}
